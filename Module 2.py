9ba6c6746ddaf07290b822612f4bb70c5c778f1e
Naoual <naoual.lahlafi@student.hu.nl>
2023-10-31 13:22:24 +0100

    alles werkt nu bijna.

--- Module 2.py
+++ Module 2.py
@@ -8,12 +8,12 @@ def gecontroleerd_bericht():
     infile = open('feedback.NS.csv', 'r')
     #reader = csv.reader(infile)
     #return reader, infile
-    berichten = infile.readlines()
+    volledig_bericht = infile.readlines()
     infile.close()
-    return berichten
+    return volledig_bericht
 
 def main():
-    berichten = gecontroleerd_bericht()
+    volledig_bericht = gecontroleerd_bericht()
     # het menu gebruik ik zodat er structuur is, hier vraag ik de naam en a-mail van de moderator.
     # hier roep ik bovenstaande variabelen aan om error te voorkomen.
     naam_moderator = input('Voer uw naam in: ')
@@ -34,20 +34,46 @@ def main():
     conn.close()
     print('Moderator: ' + str(werknemernr))
 
-    for x in berichten:
-        print('Onderstaand bericht met bijbehorende gegevens is getoond: ' + berichten[1])
+    for x in volledig_bericht:
+        print('Onderstaand bericht met bijbehorende gegevens is getoond: ' + x)
 
         controle = input('Om dit bericht goed te keuren toets ja, om dit bericht af te keuren toets nee: ')
+        outfile = open('feedback.NS.csv', 'w')
         if 'ja' in controle:
             print('Bericht is goedgekeurd!')
-        if 'nee' in controle:
+        elif 'nee' in controle:
             print('Bericht is afgekeurd!')
-        elif 'ja' and 'nee' not in controle:
+        elif 'exit' in controle:
+            return
+        else:
             print('Voer ja of nee in, er is geen andere mogelijkheid. Probeer opnieuw: ')
-        return x
+        volledig_bericht.remove(x)
+        for i in volledig_bericht:
+            outfile.write(str(i))
+        outfile.close()
+
+            #infile = open('feedback.NS.csv', 'r')
+            # reader = csv.reader(infile)
+            # return reader, infile
+            #berichten = infile.readlines()
+           # if 'ja' in controle:
+           #     controle.remove(berichten[1])
+          #  if 'nee' in controle:
+         #       controle.remove(berichten[1])
+         #   infile.close()
+    return x
             #blijf_controleren = False
 
-            #print (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+print ('Beoordeling heeft plaats gevonden op: ' + datum)
+
+
+        #if combinatie_code in code:
+          #  code.remove(combinatie_code)
+            #met een if else statement laat ik het programma de kluisnummer + wachtwoord verwijderen uit het tx bestand zodat deze weer vrij komt.
+          #  with open('fa kluizen.txt', 'w') as outfile:
+          #      for keuze in code:
+           #         outfile.write(keuze + '\n')
+            #        print('Kluis is succesvol verwijderd.')
 
 
     #het menu gebruik ik zodat er structuur is, hier vraag ik de naam en a-mail van de moderator.
