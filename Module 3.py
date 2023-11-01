a5e94c0e097d0e17b56bb193978c311a91388ff2
Naoual <naoual.lahlafi@student.hu.nl>
2023-11-01 19:08:06 +0100

    tkinter gedownload

---
+++ Module 3.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+
+
+
+
+def onclick():
+    label["text"] = entry.get()
+
+root = Tk()
+root.geometry("100x100")
+# root.config(bg='yellow')
+# img = PhotoImage(file='C:/Users/Zeliha/Downloads/img_faciliteiten/img_lift.png')
+# LBL = Label(image=img).pack()
+label = Label(master=root,
+              text='Welkom Naoual',
+
+              background='LightPink3',
+
+              foreground='gray1')
+
+entry = Entry(master=root)
+button = Button(master=root, text='Press', command=onclick)
+label.pack()
+entry.pack()
+button.pack(pady=10)
+root.mainloop()
