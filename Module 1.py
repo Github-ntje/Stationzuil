c5220ed6b8116fcda903bfa65235a655f0be3f63
Naoual <naoual.lahlafi@student.hu.nl>
2023-11-02 14:38:39 +0100

    Laatste aanpassingen module 1 + 2. Hier heb ik de opmerkingen bij module 1 en 2 geplaatst.

--- Module 1.py
+++ Module 1.py
@@ -1,7 +1,6 @@
 import csv
 import random
 import datetime
-import psycopg2
 
 datum = datetime.datetime.now().strftime("%Y-%m-%d")
 tijd =  datetime.datetime.now().strftime("%H:%M:%S")
@@ -9,21 +8,19 @@ tijd =  datetime.datetime.now().strftime("%H:%M:%S")
 
 print('Welkom, laat uw mening en/of opmerking achter over het station waar u zich op dit moment bevindt.')
 naam = input('Vul uw naam in, indien u anoniem wilt blijven vul dan niks in: ')
+#Hier vult de reiziger zijn naam in. Als hij niks invult wordt er automatisch anoniem ingevuld.
 if len(naam) <= 0:
     print('Anoniem')
 else:
     print(naam)
-
+#Met de if-statement heb ik ervoor gezorgt dat het mogelijk is om ook anoniem te kunnen blijven.
 def station():
     stations = open('Stations.txt', 'r').readline()
-
+    #Hier open ik het txt bestand van stations en lees ik het.
     stationSplitted = stations.split(",")
     random_station = random.choice(stationSplitted)
-
-    #print('station' + random_station)
+    #Met de random functie kiest het programma zelf random een station uit het bestand.
     return random_station
-    #print(f"station {random_station}")
-    #print('station' + random_station)
 def naamFunctie():
     outfile = open('feedback.NS.csv', 'a')
     if len(naam) <= 0:
@@ -34,6 +31,8 @@ def naamFunctie():
     else:
          outfile.write('Anoniem')
     outfile.close()
+    #Hier ben ik begonnen met het openen van de feedback txt bestand en heb ik 'a' gebruikt.
+    #Op deze manier wordt er de naam (of anoniem) die is ingevoerd, toegevoegd in het txt bestand.
     return naam
 
 def bericht():
@@ -49,7 +48,15 @@ def bericht():
             print('Bedankt voor uw bericht, die is verstuurt op: ')
         print(datum)
         print(tijd)
+        #Hier doe ik het zelfde als bij de naam invoegen, maar dan met de feedback.
+        #met de if-statement zorg ik ervoor dat de feedback niet langer dan 140 tekens mag bevatten.
+        #Ik gebruik hier een while loop zodat de reiziger bij te veel tekens nog een poging heeft om de feedback te versturen.
     volledig_bericht = f"{naamFunctie()},{feedback},station {station()},{datum},{tijd}\n"
+    #Hier zorg ik ervoor dat alles netjes op een rijtje in het txt bestand feedback komt te staan.
+    #Alles wordt gescheiden met komma's, zodat ik dit later nog makkelijk kan splitten indien nodig.
+    #Aan het einde heb ik \n toegevoegd zodat niet alles achter elkaar komt te staan maar elk bericht een aparte regel krijgt.
     outfile.write(volledig_bericht)
+    #Hier wordt het hele bericht met juiste structuur geschreven in het txt bestand.
     outfile.close()
+    # Uiteindelijk sluit ik het bestand.
 bericht()
--- Module 2.py
+++ Module 2.py
@@ -12,14 +12,16 @@ def gecontroleerd_bericht():
 
 def main():
     volledig_bericht = gecontroleerd_bericht()
-    # het menu gebruik ik zodat er structuur is, hier vraag ik de naam en a-mail van de moderator.
+    # het menu gebruik ik zodat er structuur is, hier vraag ik de naam en e-mail van de moderator.
     # hier roep ik bovenstaande variabelen aan om error te voorkomen.
     naam_moderator = input('Voer uw naam in: ')
     email_moderator = input('Voer uw e-mailadres in: ')
+    #moderator moet hier naam en e-mail invoeren.
 
     connection_string = "host='4.234.213.225' dbname='stationszuil' user='postgres' password='Appeltaart123!'"
     conn = psycopg2.connect(connection_string)
     cursor = conn.cursor()
+    #de connectie wordt hier gemaakt tussen de database en pycharm.
 
     moderator_check_query = """SELECT werknemernr FROM moderator WHERE naam = %s AND emailadres = %s;"""
     data = (naam_moderator, email_moderator)
@@ -28,6 +30,7 @@ def main():
     if moderator_check is None:
         query = """INSERT INTO moderator(naam, emailadres) VALUES (%s, %s);"""
         data = (naam_moderator, email_moderator)
+        #dit is de data die gebruikt moet worden voor desbtreffende kolommen
         cursor.execute(query, data)
         moderator_nummer_query = """SELECT werknemernr FROM moderator WHERE naam = %s;"""
         data = (naam_moderator,)
@@ -35,13 +38,23 @@ def main():
         werknemernummer = cursor.fetchone()[0]
     else:
         werknemernummer = moderator_check[0]
+        #hier wordt de gegeven info over moderator naar database geschreven.
+        #Verder heb ik ook select gebruikt zodat de werknemer die meerdere keren inlogt niet steeds een andere werknemernr krijgt.
+        #Als de naam en emailadres nog niet bekend is bij de database krijgt de werknemer wel een nieuwe werknemernr.
+        #Dit heb ik gedaan met de if-statement.
 
     volledig_bericht_kopie = list(volledig_bericht)
+    #Hier maak ik een kopie van volledige lijst zodat na het removen geen regels worden overgeslagen bij het lezen van het bestand.
+    #Aangezien het programma steeds vanaf index 0 begint met tellen.
+    #Hierdoor voegt het bestand steeds de regels terug toe vanaf index 0.
     for x in volledig_bericht:
         print('Onderstaand bericht met bijbehorende gegevens is getoond: ' + x)
+        #Hier wordt de feedback getoond zodat de moderator kan beoordelen.
+        # X staat hier voor 1 regel van het volledig_bericht, dus het hele feedback bestand.
+
 
         controle = input('Om dit bericht goed te keuren toets false, om dit bericht af te keuren toets true. Als u wilt stoppen met beoordelen, toets dan exit: ')
-        outfile = open('feedback.NS.csv', 'w')
+        # Hier moet de moderator het bericht goedkeuren of afgekeuren doormiddel van true en false.
 
         if 'true' in controle:
             print('Bericht is afgekeurd!')
@@ -51,17 +64,30 @@ def main():
             return
         else:
             print('Voer true of false in, er is geen andere mogelijkheid. Probeer opnieuw: ')
+        #Met de if/else-statement krijg je een reactie of het bericht juist is goedgekeurd of afgekeurd, na de beoordeling.
+        #Als een moderator wilt stoppen met beoordelen teots hij exit in.
+
+        outfile = open('feedback.NS.csv', 'w')
         volledig_bericht_kopie.remove(x)
         for i in volledig_bericht_kopie:
             outfile.write(str(i))
         outfile.close()
+        #Hier open ik feedback bestand weer, maar dan met 'w' zodat er dingen kunnen worden toegevoegd of weggeschreven.
+        #Eerst remove ik alle regels in het kopie van berichten.
+        #Daarna write ik er weer de berichten terug in die nog NIET zijn beoordeeld.
 
-        # berichten = open('feedback.NS.csv', 'r').readline()
 
         berichtensplitted = x.strip().split(",")
+        #Hier split ik in een regel van berichten bij de komma.
+        # Dit doe ik zodat ik later in dit bestand maar een deel van de regel kan weergeven.
+        # Dit doe ik met berichtensplitted[...] bij de data die getoond moet worden in de database.
+        #Als ik de berichten niet strip en split werkt dit niet.
 
         bericht_datum = datetime.datetime.strptime(berichtensplitted[3].strip(), "%Y-%m-%d")
         bericht_tijd = datetime.datetime.strptime(berichtensplitted[4].strip(), "%H:%M:%S")
+        #Hier geef ik datum en tijd een nieuwe variabelen zodat ik deze kan weergeven in de data van query INSERT INTO bericht.
+        #Als ik dit niet doe komt er in de database niet de tijd dat het bericht is verstuurd, maar de tijd dat het bericht is beoordeeld.
+        #Datum en tijd een nieuwe variabelen geven doe ik dus om de juiste tijd en datum te kunnen schrijven naar de database van berichten.
 
         query = """INSERT INTO bericht(bericht, naam, station, datum, tijd) VALUES (%s, %s, %s, %s, %s) RETURNING berichtnr;"""
         data = (berichtensplitted[1], berichtensplitted[0], berichtensplitted[2], bericht_datum, bericht_tijd)
@@ -72,18 +98,13 @@ def main():
         data = (datum, tijd, controle, werknemernummer, berichtnummer)
         cursor.execute(query, data)
         conn.commit()
+        #Hier wordt de gegeven info van beoordeling en bericht verstuurd naar de database.
+        #In de data geef ik aan welke dingen precies moeten worden toegevoegd aan de database.
     conn.close()
+    #Hier verbreek ik de connectie met de database weer.
     return x
 print ('Beoordeling heeft plaats gevonden op: ' + datum + tijd)
-
-
-
-    #het menu gebruik ik zodat er structuur is, hier vraag ik de naam en a-mail van de moderator.
-    #berichten = gecontroleerd_bericht()
-    #hier roep ik bovenstaande variabelen aan om error te voorkomen.
-
-
-
+#Uiteindelijk print ik op welke datum en tijd de beoordeling heeft plaatsgevonden.
 
 
 if __name__ == "__main__":
