31d7a5c35006a06d1ddd2d1920a08ff8ef70f01a
Naoual <naoual.lahlafi@student.hu.nl>
2023-11-06 11:45:23 +0100

    Images zijn toegevoegd, alleen nog verslag met bronvermleding toevoegen.

--- Module 3.py
+++ Module 3.py
@@ -35,9 +35,19 @@ def weer(locatie_station):
 
     weer_tekst.config(text=f"Het is momenteel: {temperatuur}°C in {locatie_station}")
     aankondigingen_tekstvak.delete('1.0', tk.END)
+
+    if locatie_station == "Nijmegen":
+        faciliteiten_nijmegen()
+    elif locatie_station == "Amsterdam":
+        faciliteiten_amsterdam()
+    elif locatie_station == "Breda":
+        faciliteiten_breda()
+
     for feedback in volledig_bericht:
-        gegeven_feedback = f"Bericht: {feedback[0]}\nNaam: {feedback[1]}\nStation: {feedback[2]}\nDatum: {feedback[3]}\nTijd: {feedback[4]}\n\n"
+        gegeven_feedback = f"\nBericht: {feedback[0]}\nNaam: {feedback[1]}\nStation: {feedback[2]}\nDatum: {feedback[3]}\nTijd: {feedback[4]}\n\n"
         aankondigingen_tekstvak.insert(tk.END, gegeven_feedback)
+    #print(feedback[2])
+
     #Hier heb ik een tekstvak gemaakt waarin het weer komt te staan.
     #Verder heb ik hier ook een for-loop gemaakt waarbij de berichten onder elkaar worden getoond.
     #Doormiddel van een f-string heb ik ervoor gezorg dat de info op goede volgorde met de juiste deel van de gehele feedback regel wordt getoond.
@@ -45,12 +55,27 @@ def weer(locatie_station):
 beginscherm = tk.Tk()
 beginscherm.title("Feedback bij stations")
 beginscherm.configure(bg='gold')
-beginscherm.geometry("800x700")
+beginscherm.geometry("1000x700")
 beginscherm.grid_rowconfigure(0, weight=3)
 beginscherm.grid_columnconfigure(0, weight=3)
+beginscherm.grid_columnconfigure(1, weight=1)
+#faciliteitenframe = tk.Frame(beginscherm)
+#faciliteitenframe.configure(background='black')
+#faciliteitenframe.grid(column=1, row=0, rowspan=18, sticky='news')
+#faciliteitenframe.grid_rowconfigure(0)
+#faciliteitenframe.grid_rowconfigure(1)
+#faciliteitenframe.grid_rowconfigure(2)
+#faciliteitenframe.grid_rowconfigure(3)
 #Hier heb ik het beginscherm gemaakt deze is geel is 800x700 en heeft als titel 'feedback bij stations'.
 
-begin_tekst = tk.Label(beginscherm, text="Welkom bij info.NS !", font= ('Arial', 15, 'bold'), bg='SkyBlue2', fg='black')
+def ns_logo():
+    global img_ns
+    img_ns = tk.PhotoImage(file='img_ns.png')
+    beginscherm.image_create(tk.NE, image=img_ns)
+  #  img_ns = tk.PhotoImage(file='img_ns.png')
+   # beginscherm.image_create(tk.W, image=img_ns)
+
+begin_tekst = tk.Label(beginscherm, text="Welkom bij informatie.NS !", font= ('Arial', 15, 'bold'), bg='SkyBlue2', fg='black')
 begin_tekst.grid(column=0, pady=5, padx=5, sticky=tk.N)
 #Vervolgens heb ik een label gemaakt met een welkoms bericht voor de reizigers, hier kunnen ze niet op klikken.
 #Deze is blauw en matcht dus met de kleuren van NS.
@@ -62,6 +87,10 @@ info_tekst.grid(row=0, column=0, pady=5, padx=5)
 weer_tekst = tk.Label(beginscherm, text="Temperatuur", font=('Arial', 11), bg='SkyBlue2', fg='black')
 weer_tekst.grid(row=2, column=0, pady=10, padx=10)
 #Op deze label komt de temperatuur van het gekozen station te staan.
+
+#aankondigingen_tekstvak = tk.Text(beginscherm, height=20, width=100, font= ('Arial', 10), bd=5, bg='snow', relief='groove')
+#aankondigingen_tekstvak.grid(row=index+2, column=0, pady=10, padx=10)
+#Hier heb ik de tekstvak gemaakt waar alle berichten in komen te staan.
 
 plaatsen = ["Breda", "Nijmegen", "Amsterdam"]
 for index, plaats in enumerate(plaatsen, start=2):
@@ -77,14 +106,6 @@ aankondigingen_tekstvak = tk.Text(beginscherm, height=20, width=100, font= ('Ari
 aankondigingen_tekstvak.grid(row=index+2, column=0, pady=10, padx=10)
 #Hier heb ik de tekstvak gemaakt waar alle berichten in komen te staan.
 
-#def ns_logo():
-    #global img_ns
-    #img_ns = tk.PhotoImage(beginscherm, image='img_ns.png')
-    #positie= (x==20, y==20)
-    #beginscherm.image_create(positie, image='img_ns')
-  #  img_ns = tk.PhotoImage(file='img_ns.png')
-   # beginscherm.image_create(tk.W, image=img_ns)
-
 
 def faciliteiten_breda():
     global img_lift, img_pr
@@ -93,15 +114,8 @@ def faciliteiten_breda():
     img_pr = tk.PhotoImage(file='img_pr.png')
     aankondigingen_tekstvak.image_create(tk.END, image=img_pr)
     #Hier gebruik ik een functie om de bijbehorende images toe te voegen.
-    #De images verschijnen in de tekstvak.
+    #De images verschijnen in de tekstvak zodra je op dknop van desbtreffende station drukt.
     #Voor dit gedeelte heb ik video's opgezocht op het internet.
-
-
-faciliteit = tk.Button(beginscherm, text="Faciliteiten Breda ", command=faciliteiten_breda, font=('Arial', 10, 'bold'), bg='skyblue2',
-                           fg='black')
-faciliteit.grid(row=1, column=0, pady=5, padx=10, sticky=tk.E)
-faciliteit.place(x=140, y=120)
-#Hier maak ik een knop aan waar de reiziger op kan klikken om desbetreffende faciliteiten te zien, hier dus van Breda.
 
 def faciliteiten_nijmegen():
     global img_toilet, img_ovfiets
@@ -110,12 +124,8 @@ def faciliteiten_nijmegen():
     img_ovfiets = tk.PhotoImage(file='img_ovfiets.png')
     aankondigingen_tekstvak.image_create(tk.END, image=img_ovfiets)
     #Hier gebruik ik weer een functie om de images te laten verschijnen in het tekstgedeelte.
+    # Dit gebeurt zodra je op de knop van desbtreffende station drukt.
 
-
-faciliteit1 = tk.Button(beginscherm, text="Faciliteiten Nijmegen ", command=faciliteiten_nijmegen, font=('Arial', 10, 'bold'), bg='skyblue2', fg='black')
-faciliteit1.grid(row=1, column=1, pady=5, padx=10, sticky=tk.E)
-faciliteit1.place(x=320, y=120)
-#Dit is ook een knop die de faciliteiten laat zien van het station, hier is dat dus Nijmegen.
 def faciliteiten_amsterdam():
     global img_lift, img_pr
     img_lift = tk.PhotoImage(file='img_lift.png')
@@ -123,22 +133,6 @@ def faciliteiten_amsterdam():
     img_pr = tk.PhotoImage(file='img_pr.png')
     aankondigingen_tekstvak.image_create(tk.END, image=img_pr)
     aankondigingen_tekstvak.delete(tk.END)
-    #Wederom maak ik gebruik van een functie zodat de images in het tekstvak komen te staan.
-
-faciliteit2 = tk.Button(beginscherm, text="Faciliteiten Amsterdam ", command=faciliteiten_amsterdam, font=('Arial', 10, 'bold'), bg='skyblue2', fg='black')
-faciliteit2.grid(row=1, column=2, pady=5, padx=10, sticky=tk.E)
-faciliteit2.place(x=500, y=120)
-#Ook hier maak ik een knop, maar dan voor de faciliteiten van Amsterdam.
-#Zodra de reiziger hierop drukt verschijnen de images.
-
-def tekstbox_legen():
-    aankondigingen_tekstvak.delete("1.0","end")
-knop_verwijderen=tk.Button(beginscherm, height=1, width=15, text="Tekstbox legen ", command=tekstbox_legen, font=('Arial', 10, 'bold'), bg='blue2',
-                           fg='snow')
-knop_verwijderen.grid()
-#Ik heb gebruik gemaakt van een verwijder knop, deze hoeft de reiziger alleen te gebruiken na het intikken van de faciliteiten knop.
-#Dit is er zodat de images niet allemaal door elkaar gaan staan.
-#Het is dus belangrijk om deze steeds in te drukken na het toetsen van een faciliteiten knop.
-#Deze knop maakt het tekstvak weer helemaal leeg.
+    #Wederom maak ik gebruik van een functie zodat de images in het tekstvak komen te staan na het klikken op de knop.
 
 beginscherm.mainloop()
